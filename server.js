18e22d1e2b596f529214f3f2e235c714c129cfce
Rosy122333 <80847580+Rosy122333@users.noreply.github.com>
2024-10-02 16:34:30 -0400

    css and linking

---
+++ server.js
@@ -0,0 +1,38 @@
+const http = require('http'),
+      fs   = require('fs'),
+      port = 3000
+
+const server = http.createServer( function( request,response ) {
+  switch( request.url ) {
+    case '/':
+      sendFile( response, 'public/login.html' )
+      break
+    case '/index.html':
+      sendFile( response, 'public/login.html' )
+      break
+    case '/signup.html':
+      sendFile( response, 'public/signup.html' )
+      break
+    case '/style.css':
+      sendFile( response, 'public/style.css' )
+      break
+      case '/createAccount': //will change this when database is avaliable
+        sendFile( response, 'public/signup.html' )
+        break
+        case '/login'://will change this when database is avaliable
+            sendFile( response, 'public/index.html' )
+            break
+    default:
+      response.end( '404 Error: File Not Found' )
+  }
+})
+
+server.listen( process.env.PORT || port )
+
+const sendFile = function( response, filename ) {
+   fs.readFile( filename, function( err, content ) {
+     response.end( content, 'utf-8' )
+   })
+}
+
+
