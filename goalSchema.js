305d1374e71d7a55d879c6aca042eab73342c78e
LauraPellowski <lmpellowski@gmail.com>
2024-10-04 15:51:22 -0400

    started defaults for goals

--- goalSchema.js
+++ goalSchema.js
@@ -1,13 +1,25 @@
 const mongoose = require('mongoose')
 
 const goalSchema = new mongoose.Schema({
-    author: mongoose.SchemaTypes.ObjectId,
-    title: String,
+    author: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: [true, 'author id is required to link a goal to its user']
+    },
+    title: {
+        type: String,
+        default: "My Goal",
+    },
     description: String,
     dueDate: Date,
     priority: String,
-    complete: Boolean,
-    active: Boolean
+    complete: {
+        type: Boolean,
+        default: false,
+    },
+    active: {
+        type: Boolean,
+        default: false
+    }
 });
 
 const Goal = mongoose.model('Goal', goalSchema);
