f2f3faf3ca73202d591b5a28d08717fc13de0200
Ashley Jacob <128179385+Asjacob25@users.noreply.github.com>
2024-10-08 12:58:28 -0400

    updates to goals

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const http = require('http'),
 const client = new MongoClient( uri )
 const db = client.db("test")
 let userCollection = null
+let goalsCollection = null
 let collection = null
 
 
@@ -45,6 +46,7 @@ async function run() {
   //my database here collecction variable
   collection = await client.db("test").collection("namey") //default cause otherwise get 503 error  in app.use
   userCollection = await client.db("test").collection("users")
+  goalsCollection = await client.db("test").collection("goals")
 
 
 
@@ -77,30 +79,30 @@ app.use( express.urlencoded({ extended:true }) )
 
 
 //not referencing this at all cause I commented it out
-async function testDatabase() {
- let user = await User.exists({ username: "Lauratest3" })
- if (user == null) {
-   user = await User.create ({
-     username: "Lauratest3",
-     password: "password",
-     goals: [],
-     points: 0,
-     leaderboardNumber: null
-   })
- }
-  const goal = await Goal.create ({
-   author: user._id,
-   title: "GOAL",
-   description: "fdsfdsfdsf",
-   dueDate: "2024-10-04",
-   priority: "low",
-   complete: false,
-   active: false
- })
+// async function testDatabase() {
+//  let user = await User.exists({ username: "Lauratest3" })
+//  if (user == null) {
+//    user = await User.create ({
+//      username: "Lauratest3",
+//      password: "password",
+//      goals: [],
+//      points: 0,
+//      leaderboardNumber: null
+//    })
+//  }
+//   const goal = await Goal.create ({
+//    author: user._id,
+//    title: "GOAL",
+//    description: "fdsfdsfdsf",
+//    dueDate: "2024-10-04",
+//    priority: "low",
+//    complete: false,
+//    active: false
+//  })
 
 
- await User.findOneAndUpdate( { _id: user._id}, { $push: {goals: goal._id}})
-}
+//  await User.findOneAndUpdate( { _id: user._id}, { $push: {goals: goal._id}})
+// }
 //testDatabase();
 
 app.engine( 'handlebars',  hbs() )
@@ -120,6 +122,18 @@ app.use( cookie({
 app.post( '/createAccount', (req,res)=> {
  res.redirect( 'signup.html' )
 })
+
+app.get('/goals', async (req, res) => {
+  
+  if (!req.session.login) {
+    return res.status(401).send('Unauthorized');
+  }
+
+  const userId = req.session.userId;
+  const goals = await goalsCollection.find({ author: ObjectId(userId) }).toArray();
+
+  res.json(goals);
+});
 
 app.post( '/newAccount', async (req,res)=> {
 
@@ -209,6 +223,11 @@ app.post( '/home', (req,res)=> {
 
 app.post( '/leaderboard', (req,res)=> {
   res.redirect( 'leaderboard.html' )
+  //would also rechange a global variable if we start saving a global variable which knows what user is logged in
+ })
+
+ app.post( '/goals', (req,res)=> {
+  res.redirect( 'goals.html' )
   //would also rechange a global variable if we start saving a global variable which knows what user is logged in
  })
 
