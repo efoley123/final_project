9cb3cf1d50c84d9a25e695fa7da228dc1ed11855
Rosy122333 <80847580+Rosy122333@users.noreply.github.com>
2024-10-08 17:06:43 -0400

    need to get the goals for that day

--- public/main.js
+++ public/main.js
@@ -1,20 +1,31 @@
 
-
-
 const date = async function( event ) {
     const currentDate = new Date().toDateString();// Outputs "Mon Aug 31 2020"
-
     console.log(currentDate);
     //textContent
-
     DateForToday.textContent = currentDate;
+}
+
+const goalsToday = async function( event ) {
+
+    //will query for the goals of the current day
+    //date should be defined already
+    // have to see what the professor did before with his code
 
 }
+
+
 
 
 window.onload = function() {
     date();
 
     const DateForToday = document.getElementById('DateForToday');
+
+    goalsToday();
+    console.log("ran this above");
+
+
+    const tableBody = document.getElementById('tableBody');
 
 }
